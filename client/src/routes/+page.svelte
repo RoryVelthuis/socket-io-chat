d145279888939388a490542522dd0adef7545051
RoryVelthuis <RorySVelthuis@gmail.com>
2024-09-23 00:25:07 +1200

    client folder should no longer be a submodule

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from 'svelte';
+    import { io } from 'socket.io-client';
+
+    let socket;
+    let connected = false;
+
+    onMount(() => {
+        socket = io('http://localhost:3000');
+        socket.on('connect', () => {
+            connected = true;
+            console.log('Connected to server');
+        });
+    });
+</script>
+
+<div>
+    {#if connected}
+        <h1>Connected to server</h1>
+    {:else}
+        <h1>Connecting to server...</h1>
+    {/if}
+</div>
