b6550ae3f7d735f17b5a94b0c9261b927728e23c
RoryVelthuis <RorySVelthuis@gmail.com>
2024-09-23 15:29:55 +1200

    Added features: -Server holds list of users -Updated client UI to display users -Users can set a name

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
     import { onMount } from 'svelte';
     import { io } from 'socket.io-client';
-    import { messages } from '$lib/store';
+    import { messages, currentUsers } from '$lib/store';
 
     let socket;
     let message = '';
     let recipient = '';
 
+    let nameInput = ''
+    let name =''
+
     function sendMessage(event) {
         // Send the message to the server
-        socket.emit('chatMessage', { message: message});
+        socket.emit('chatMessage', message);
         message = '';
     }
 
@@ -27,30 +30,56 @@
         }
     }
 
+    function changeName(event) {
+        socket.emit('changeName', nameInput);
+    }
+
     onMount(() => {
         // Connect to the server
         socket = io('http://localhost:3000');
+
+        // Retrieve user info from localStorage
+        const storedName = localStorage.getItem('name');
+        if (storedName) {
+            name = storedName;
+            socket.emit('changeName', name);
+        }
 
         // Listen for the 'welcome' event from the server
         socket.on('welcome', (data) => {
             console.log(data.message);  // Outputs: "Welcome to the game!"
         });
 
+        // Listen for the 'userList' event from the server
+        socket.on('userList', (data) => {
+            console.log('User list:', data);
+            currentUsers.set(data);
+        });
+
+        // Listen for the 'nameChanged' event from the server
+        socket.on('nameChanged', (data) => {
+            name = data.name;
+            localStorage.setItem('name', data.name);
+            console.log(`Name changed to: ${data.name}`);
+        });
+
         // Listen for the 'chat message' event from the server
         socket.on('chatMessage', (message) => {
             console.log(`New message from: [ ${message.playerId} ]: ${message.message}`);
-            messages.update(messages => [...messages, { type: 'chat', playerId: message.playerId, message: message.message, timestamp: message.timestamp } ]);
+            messages.update(messages => [...messages, { type: 'chat', playerId: message.playerId, message: message.message, timestamp: message.time } ]);
         });
 
         // Listen for the 'private message' event from the server
         socket.on('privateMessage', (message) => {
             console.log(`New private message from: [ ${message.playerId} ]: ${message.message}`);
-            messages.update(messages => [...messages, { type: 'private', playerId: message.playerId, message: message.message, timestamp: message.message } ]);
+            messages.update(messages => [...messages, { type: 'private', playerId: message.playerId, message: message.message, timestamp: message.time } ]);
         });
     });
 </script>
 
 <div>
+    <input type="text" class="set-name-input" bind:value={nameInput} placeholder="Set name">
+    <button class="set-name-button" on:click={changeName}>Set Name</button>
     <!-- Chat box -->
     <h2>Chat</h2>
     <!-- Display chat messages -->
@@ -58,9 +87,9 @@
         {#each $messages as msg}
             <div>
                 {#if msg.type === 'chat'}
-                    [ {msg.playerId} ]: {msg.message}
+                    [ {msg.timestamp} ] [ {msg.playerId} ]: {msg.message}
                 {:else}
-                    [ {msg.playerId} ] (private): {msg.message}
+                    [ {msg.timestamp} ] [ {msg.playerId} ]: (private): {msg.message}
                 {/if}
             </div>
         {/each}
@@ -70,9 +99,89 @@
     <input class="chat-input" type="text" bind:value={message} placeholder="Enter your message" />
     <button on:click={handleSend}>Send</button>
 
+    <h2>Current Users</h2>
+    <ul class="current-user-list">
+        <li class="current-user-header">
+            <div class="user-id-header">ID</div>
+            <div class="user-name-header">Name</div>
+        </li>
+        {#each $currentUsers as user}
+            <li class="current-user">
+                <div class="user-id">[ {user.id} ]</div>
+                <div class="user-name">{user.name}</div>
+            </li>
+        {/each}
+    </ul>
+
 </div>
 
 <style>
+    /* User List */
+    .current-user-list {
+        list-style-type: none;
+        border: 1px solid #ccc;
+        height: 200px;
+        overflow-y: scroll;
+        width: fit-content;
+    }
+
+    .current-user-header {
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        display: flex;
+        flex-direction: row;
+        padding: 5px 0px;
+        background-color: #f0f0f0;
+        font-weight: bold;
+        border-bottom: 1px solid #ccc;
+        cursor: default;
+    }
+
+    .current-user{
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+    .current-user:hover {
+        background-color: #ebebeb;
+    }
+
+    .user-id-header {
+        font-family: 'Courier New', Courier, monospace; /* Use a monospaced font */
+        min-width: 26ch; /* Fixed width to fit up to 16 characters */
+        padding: 0px 10px 0px 5px;
+        font-weight: bold;
+        color: #000000;
+        border-right: 1px solid #ccc;
+    }
+    .user-id {
+        font-family: 'Courier New', Courier, monospace; /* Use a monospaced font */
+        min-width: 26ch; /* Fixed width to fit up to 16 characters */
+        padding: 0px 10px 0px 5px;
+        font-weight: bold;
+        color: #000000;
+        border-right: 1px solid #ccc;
+    }
+    .user-id:hover {
+        background-color: #dfdfdf;
+    }
+
+
+    .user-name-header {
+        padding: 0px 10px;
+        min-width: 22.5ch;
+        border-right: 1px solid #ccc;
+    }
+    .user-name {
+        min-width: 24ch; /* Fixed width to fit up to 16 characters */
+        padding: 0px 10px;
+        border-right: 1px solid #ccc;
+    }
+    .user-name:hover {
+        background-color: #dfdfdf;
+        cursor: default;
+    }
+
+    /* Chat */
     .chat-box {
         height: 200px;
         overflow-y: scroll;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors'); // Import the CORS package
+const e = require('express');
 
 const app = express();
 const server = http.createServer(app);
@@ -19,31 +20,64 @@ const io = new Server(server, {
   }
 });
 
+const users = {}; // Store the connected users
+
+function logAllCurrentUsers() {
+  console.log(`Current users (${Object.keys(users).length}):`);
+  for (const [id, user] of Object.entries(users)) {
+    console.log(`[ ${id} ]: ${user.name}`);
+  }
+}
+
+function emitUserList() {
+  io.emit('userList', Object.values(users));
+}
+
 // When a client connects
 io.on('connection', (socket) => {
   console.log(`Client connected: ${socket.id}`);
 
+  // Store the connected user
+  users[socket.id] = { id: socket.id, name: `User-${socket.id}` };
+
   // Send a welcome message to the newly connected client
-  socket.emit('welcome', { message: 'Welcome to the game!', playerId: socket.id });
+  socket.emit('welcome', { message: ` User [ ${socket.id} ] joined. Welcome! `, playerId: socket.id });
+
+  // Emit the updated user list to all clients
+  emitUserList();
+
+  // Log the current user list
+  logAllCurrentUsers();
+
+
+  // Listen to name change requests from this client
+  socket.on('changeName', (newName) => {
+      console.log(`User [ ${socket.id} ] changed name to: ${newName}`);
+      users[socket.id].name = newName;
+      socket.emit('nameChanged', { playerId: socket.id, name: newName });
+      emitUserList();
+      logAllCurrentUsers();
+  });
 
   // Listen to chat messages from this client
-  socket.on('chatMessage', (data) => {
+  socket.on('chatMessage', (message) => {
     let timestamp = new Date().toISOString(); // Generate the current timestamp
-    let message = data.message;
     console.log(`Message from: [ ${socket.id} ]: ${message}`);
-    io.emit('chatMessage', { playerId: socket.id, message: message, timestamp: timestamp });
+    io.emit('chatMessage', { playerId: socket.id, message: message, time: timestamp });
   });
 
   // Listen to private messages from this client
   socket.on('privateMessage', ({ to, message }) => {
     let timestamp = new Date().toISOString(); // Generate the current timestamp
-    io.to(to).emit('privateMessage', { playerId: socket.id, message: message, timestamp: timestamp });
+    io.to(to).emit('privateMessage', { playerId: socket.id, message: message, time: timestamp });
 });
 
   // Broadcast a global message to all clients when someone disconnects
   socket.on('disconnect', () => {
     console.log(`Client disconnected: ${socket.id}`);
     io.emit('playerDisconnected', { playerId: socket.id });
+    delete users[socket.id]; // Remove the user from the users list
+    logAllCurrentUsers();
   });
 });
 
